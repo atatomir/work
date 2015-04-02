161dd106a667bff24a8eaa68937afc0376e6aea1
atatomir <atatomir@users.noreply.github.com>
2015-04-02 22:25:14 +0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Start script"
+for entry in `ls`; do 
+    if [ -d $entry ] ; then 
+        for src in `ls $entry`; do 
+            echo ----------------------------- $entry ---------------------------
+            g++ $entry/$src -o mytest 
+            echo ----------------------------------------------------------------
+            echo " "  
+        done
+    fi
+done
