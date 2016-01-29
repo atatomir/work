ed939f169688f43095050a951f347395f0bc90cb
atatomir <atatmir5@gmail.com>
2016-01-29 12:12:35 +0200

    test-add

---
+++ test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <cmath>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+#define pb push_back
+#define mp make_pair
+#define ll long long
+
+int main()
+{
+    freopen("*.in", "r", stdin);
+    freopen("*.out", "w", stdout);
+
+    return 0;
+}
